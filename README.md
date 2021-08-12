e54a0955b4c724ed63d22e4d3c6849f234a53f61
Thomas Radloff <bostontrader@gmail.com>
2021-08-12 09:10:37 +0800

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# graf2hwsmn
-The purpose of graf2hwsmn is to provide a means to POST Grafana alerts to the Huawei Cloud Simple Message Notification (SMN) service.
+# Introduction to graf2hwsmn
+
+The purpose of graf2hwsmn is to provide a means to POST Grafana alerts to the Huawei Cloud Simple Message Notification 
+(SMN) service.
+
+Although the original use case was to get Grafana to send alerts via SMS, the SMN service can be configured much more 
+broadly than that.
+
+We need graf2hwsmn because although Grafana gives us the ability to POST alerts to a URL, that's not enough to actually 
+send messages via the SMN service.  In order to actually do this, we have to jump through a lot of hoops with dealing 
+with Access Keys, Secret Keys, signing the request, putting signatures in headers more!.
+
+graf2hwsmn bridges that gap.  With graf2hwsmn you can:
+
+* Create a configuration file that contains your sensitive Huawei credentials.
+
+* Specify the location of said configuration file via command-line argument.
+
+* Launch graf2hwsmn via systemd.
+
+Given these powers, Grafana notification channels can POST a request that will ultimately get sent by the Huawei SMN 
+service.
+
+A caveat... graf2hwsmn is not a general-purpose bridge from any random source that might want to POST to Huawei SMN.  
+Instead, it is specifically built only for Grafana.  That said, it shouldn't be very difficult to modify or generalize 
+to handle other sources.  I leave doing so as an exercise for the reader.
+
+# Security
+
+There are basically two parts to consider when contemplating the security of this:
+
+* Grafana will POST HTTP to graf2hwsmn running somewhere.
+
+* graf2hwsmn will then POST HTTPS to some endpoint in the Huawei cloud.
+
+The link from graf2hwsmn to Huawei cloud is the easy part to secure.  It's done via https and uses their system of 
+Access Keys, Secret Keys, and request signing to do it's work.  Good luck hacking that.  That's probably not the weak 
+link.
+
+The link from Grafana to graf2hwsmn is the troublesome part.  By default Grafana will POST HTTP to graf2hwsmn.  No 
+TLS and no authentication.  If graf2hwsmn is running on the same machine as Grafana then this is a lesser problem.
+
+If you want to run graf2hwsmn on another machine then one obvious method of increasing security would be to use
+ssh port forwarding.
+
+Another possible step to increase this security would be to run graf2hwsmn behind nginx configured to use TLS or 
+something similar.
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"./core"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// https://support.huaweicloud.com/en-us/api-smn/en-us_topic_0036017316.html
+
+type Config struct {
+	AccessKey string
+	SecretKey string
+	EndPoint  string
+	ProjectID string
+	TopicURN  string
+}
+
+func main() {
+
+	configFileNameDefault := "grafana2huaweismn.json"
+	var configFileName string
+	flag.StringVar(&configFileName, "c", configFileNameDefault,
+		fmt.Sprintf("The name of a config file to use.  Default is %s", configFileNameDefault))
+
+	flag.Parse()
+
+	// Now read and parse the config file
+	content, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var config Config
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	//http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/", requestHandlerWrapper(config))
+
+	fmt.Printf("Starting grafana2huaweismn server using configFile = %s\n", configFileName)
+	if err := http.ListenAndServe(":9112", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// This is how we feed the configuration into the handler
+func requestHandlerWrapper(config Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			// If error, do nothing, just return.
+			return
+		}
+		fmt.Printf("Incoming request...\n")
+
+		switch r.Method {
+
+		case "POST":
+			fmt.Printf("Incoming POST request.\n")
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				// If error, do nothing, just return.
+				return
+			}
+
+			// Unmarshal json of unknown structure and pick out a field named "message"
+			// This is useful for Grafana, but you might need to dissect this for your application.
+			var arr map[string]interface{}
+			if err := json.Unmarshal(reqBody, &arr); err != nil {
+				// If error, do nothing, just return.
+				return
+			}
+			var msg = arr["message"]
+
+			send2Huawei(msg, config)
+
+		default:
+			fmt.Fprintf(w, "Only the POST method is supported.")
+		}
+	}
+}
+
+func send2Huawei(message2Send interface{}, config Config) {
+
+	//Set the AK/SK to sign and authenticate the request.
+	s := core.Signer{
+		Key:    config.AccessKey,
+		Secret: config.SecretKey,
+	}
+
+	// Convert the message2Send that we have now, to a []byte json that huawei wants.
+	type JString struct {
+		Message string `json:"message"`
+	}
+
+	j := JString{
+		Message: fmt.Sprintf("%v", message2Send),
+	}
+	j1, _ := json.Marshal(j)
+
+	// https://support.huaweicloud.com/en-us/api-smn/PublishMessage.html
+	// {URI-scheme}://{Endpoint}/{resource-path}?{query-string}
+	resourcePath := fmt.Sprintf("v2/%s/notifications/topics/%s/publish", config.ProjectID, config.TopicURN)
+	url := fmt.Sprintf("https://%s/%s", config.EndPoint, resourcePath)
+	r, _ := http.NewRequest("POST", url, bytes.NewBuffer(j1))
+	fmt.Println(url)
+
+	r.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	s.Sign(r)
+	fmt.Println(r.Header)
+	client := http.DefaultClient
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(body))
+}
